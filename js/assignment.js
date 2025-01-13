14a2c0b2eda6bb96003ecd7f2dfbb867a8c8351a
Bri A <alexiellis22@gmail.com>
2025-01-13 16:37:58 -0500

    Render answers/Update README.md

--- js/assignment.js
+++ js/assignment.js
@@ -1,46 +1,46 @@
 "use strict";
 
-/* const submissionBtn = document.getElementById("submission-btn");
+const submissionBtn = document.getElementById("submission-btn");
 const smallestNumberElement = document.getElementById("smallest-number");
 const largestNumberElement = document.getElementById("largest-number");
-const averageNumberElement = document.getElementById("average-number"); */
+const averageNumberElement = document.getElementById("average-number");
 
 const myNumbers = [35, 130, 354, 100, 25, 134, 99, 200, 35];
 
-
-
 function findSmallestNumber(array) {
-    let smallestNumber = array[0];
+  let smallestNumber = array[0];
   for (let i = 1; i < array.length; i++) {
-    if  (array[i] < smallestNumber) {
+    if (array[i] < smallestNumber) {
       smallestNumber = array[i];
     }
+  }
+  return smallestNumber;
 }
-return smallestNumber;
-}
-
-console.log(findSmallestNumber(myNumbers));
-
 
 function findLargestNumber(array) {
-    let largestNumber = array[0];
-    for (let i = 1; i < array.length; i++) {
-       if(array[i] > largestNumber){
-          largestNumber = array[i];     
+  let largestNumber = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] > largestNumber) {
+      largestNumber = array[i];
     }
+  }
+  return largestNumber;
 }
-return largestNumber;
-}
-
-console.log(findLargestNumber(myNumbers));
 
 function findAverage(array) {
-    let sum = 0;
-    for (let i = 0; i < array.length; i++) {
-       sum += array[i];
-        
-    }
-    return sum/array.length;
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum / array.length;
 }
 
-console.log(findAverage(myNumbers));
+function render() {
+  smallestNumberElement.innerText = findSmallestNumber(myNumbers);
+  largestNumberElement.innerText = findLargestNumber(myNumbers);
+  averageNumberElement.innerText = findAverage(myNumbers);
+}
+
+submissionBtn.addEventListener("click", function () {
+  render();
+});
