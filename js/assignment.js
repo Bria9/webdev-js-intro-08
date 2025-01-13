414c316d3abeb8dfad2e0a116f943b7ba0823f46
Bri A <alexiellis22@gmail.com>
2025-01-13 15:55:58 -0500

    Create findSmallestNumber function

--- js/assignment.js
+++ js/assignment.js
@@ -1,8 +1,23 @@
 "use strict";
 
-const submissionBtn = document.getElementById("submission-btn");
+/* const submissionBtn = document.getElementById("submission-btn");
 const smallestNumberElement = document.getElementById("smallest-number");
 const largestNumberElement = document.getElementById("largest-number");
-const averageNumberElement = document.getElementById("average-number");
+const averageNumberElement = document.getElementById("average-number"); */
 
 const myNumbers = [35, 130, 354, 100, 25, 134, 99, 200, 35];
+
+
+
+function findSmallestNumber(array) {
+    let smallestNumber = array[0];
+  for (let i = 1; i < array.length; i++) {
+    if  (array[i] < smallestNumber) {
+      smallestNumber = array[i];
+    }
+}
+return smallestNumber;
+}
+
+console.log(findSmallestNumber(myNumbers));
+
